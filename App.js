e79daf90377d27881ec8a8a2ec6bf0923ef6298a
Jose <bestchallenge14@gmail.com>
2026-06-21 19:33:58 -0500

    Added Employee List and Router

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
 import EmployeeForm from './components/EmployeeForm';
 
+// Week 4: Employee List Component
+function EmployeeList(props) {
+  // Render the employee list
+  return (
+    <div className="employee-list">
+      <h1>Employee List</h1>
+      <ul>
+        {props.employees.map((employee) => (
+          <li key={employee.EmployeeId}>
+            {/* Create a link to the employee detail page */}
+            <Link to={`/employees/${employee.EmployeeId}`}>
+              {employee.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// Main App Component
 function App() {
   const [employees, setEmployees] = useState([]);
 
@@ -26,10 +48,15 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {/* Pass the addEmployee function as a prop to the form */}
-      <EmployeeForm addEmployee={addEmployee} />
-    </div>
+    <Router>
+      <div className="App">
+        {/* Pass the addEmployee function as a prop to the form */}
+        <EmployeeForm addEmployee={addEmployee} />
+        
+        {/* Display the Employee List */}
+        <EmployeeList employees={employees} />
+      </div>
+    </Router>
   );
 }
 
